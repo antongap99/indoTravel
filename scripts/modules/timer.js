4f6b6dc2d0ad584f8297b59cde21e0dfe16ebbb4
Anton.gap <anton.gap99@gmail.com>
2022-08-05 13:49:13 +0300

    feat: add seconds when dayscount is zero

--- scripts/modules/timer.js
+++ scripts/modules/timer.js
@@ -17,12 +17,15 @@ const timer = (deadline , timerWrapper) => {
         const minutes = Math.floor(timeRemaining / 1000/ 60 % 60 );
         const hours = Math.floor(timeRemaining / 1000/ 60 / 60 % 24);
         const days = Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
+        const seconds = Math.floor(timeRemaining / 1000 % 60);
+        
 
         return {
             timeRemaining,
             days,
             minutes,
             hours,
+            seconds,
         }
     }
 
@@ -92,11 +95,54 @@ const timer = (deadline , timerWrapper) => {
                 timerUnitsMinutes.textContent = 'минут';
             }
         }
+        const unitSecondModify = () => {
+            if((timer.seconds === 1 
+                || timer.seconds % 10 === 1) &&
+                Math.floor(timer.seconds / 10) !== 1
+            ) {
+                timerUnitsMinutes.textContent = 'секунда'
+            }else if ((1 < timer.seconds &&  timer.seconds < 5 
+                || 1 < timer.seconds  % 10  &&  timer.seconds % 10 < 5) && 
+            (Math.floor(timer.seconds / 10) !== 1) ) {
+
+                timerUnitsMinutes.textContent = 'секунды';
+            } else if ( timer.seconds >= 5 
+                || timer.seconds % 10 >= 5 
+                || timer.seconds === 0
+                || timer.seconds % 10 === 0
+                
+                ) {
+                timerUnitsMinutes.textContent = 'секунд';
+            }
+        }
         unitDayModify();
         unitHourModify();
         unitMinuteModify();
+
+        const timerFormatToggle = () => {
+            if (timer.days === 0) {
+                timerCountDays.classList.add('zeroday', 'zeroday__count_hours');
+                timerUnitsDays.classList.add('zeroday', 'zeroday__unit_hours');
+
+                timerCountHours.classList.add('zeroday', 'zeroday_count_minutes');
+                timerUnitsHours.classList.add('zeroday', 'zeroday_unit_minutes');
+                
+                timerCountMinutes.classList.add('zeroday', 'zeroday_count_seconds');
+                timerUnitsMinutes.classList.add('zeroday', 'zeroday_unit_seconds');
+
+
+                timerCountDays.textContent = timerCountHours.textContent;
+                timerUnitsDays.textContent = timerUnitsHours.textContent;
+                timerCountHours.textContent = timerCountMinutes.textContent; 
+                timerUnitsHours.textContent = timerUnitsMinutes.textContent; 
+                timerCountMinutes.textContent = timer.seconds; 
+                timerUnitsMinutes.textContent = 'секунд'; 
+                unitSecondModify();
+            }
+        }
         
-        const intervalId = setTimeout(startTimeRemaining, 10000);
+        timerFormatToggle();
+        const intervalId = setTimeout(startTimeRemaining, 1000);
 
          if(timer.timeRemaining <= 0 ) {
 
@@ -120,11 +166,14 @@ const timer = (deadline , timerWrapper) => {
 
 const startTimer = () => {
     const timerWrapper = document.querySelector('.timer');
-try {
-    timer(timerWrapper.dataset.timerDeadline, timerWrapper);
-} catch (error) {
-    console.log('акция закончилась');
-}
+    try {
+        timer(timerWrapper.dataset.timerDeadline, timerWrapper);
+    } catch (error) {
+        
+    }
+   
+
+
     
 
    
