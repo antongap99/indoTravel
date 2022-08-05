34b4f40e40c7c6e92aff4780d0d4998980aedd92
Anton.gap <anton.gap99@gmail.com>
2022-08-05 18:16:13 +0300

    feat: add closing burger menu

--- scripts/modules/burgerMenu.js
+++ scripts/modules/burgerMenu.js
@@ -5,7 +5,19 @@ const burgerControl = () => {
     burgerBtn.addEventListener('click', () => {
         headerMenu.classList.toggle('header__menu_active');
     })
+
+    document.addEventListener('click', (e) => {
+        if((!e.target.matches('.header__menu') && !e.target.matches('.header__list')) && 
+        !e.target.matches('.header__menu-button')
+        && headerMenu.classList.contains('header__menu_active') || e.target.matches('.header__item')){
+            headerMenu.classList.remove('header__menu_active');
+        }
+    })
 }
+
+
+
+
 
 export default {
     burgerControl,
