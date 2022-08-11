bded7dba0f2a6ce530960e42156df4f859409c5b
Anton.gap <anton.gap99@gmail.com>
2022-08-11 14:39:34 +0300

    feat: add rotate plane

--- scripts/modules/airplane.js
+++ scripts/modules/airplane.js
@@ -24,23 +24,52 @@ window.addEventListener('resize', () => {
 
 
     
-const calcPossitionAirPlane = () => {
+const calcPositionAirPlane = (prevScroll) => {
+    console.log('prevScroll: ', prevScroll);
     const maxDistance = document.documentElement.clientHeight - airPlane.clientHeight;
-
     const maxSroll = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
-    const persentScroll = (window.scrollY * 100) / maxSroll;
+    return () => {
+        let persentScroll = (window.scrollY * 100) / maxSroll;
 
+            let currentScroll = window.scrollY;
+    
+        console.log('1 ', currentScroll);
+        
     const flyPosition = maxDistance * (persentScroll / 100);
-
     airPlane.style.transform = `translateY(${-flyPosition}px)`;
+
+    if(currentScroll < prevScroll)  {
+        airPlane.style.transform += ` rotate(180deg)`;
+         console.log(' airPlane.style.transform: ',  airPlane.style.transform);
+      }
+
+    return currentScroll;      
+
+    }
+  
+      
 }
+
+const debounce = (fn, raf = NaN) => {
+    let prevScroll;
+    return () =>{
+        if(raf) return;
+        raf = requestAnimationFrame(() =>{
+            prevScroll = fn(prevScroll)();
+            raf = NaN;
+        })
+    }
+};
+
+
+const calcPositionAirPlaneDebounce = debounce(calcPositionAirPlane);
 
 
 
 
 window.addEventListener('scroll', () => {
-    requestAnimationFrame(calcPossitionAirPlane);
+    requestAnimationFrame(calcPositionAirPlaneDebounce);
 })
 
-calcPossitionAirPlane();
+// calcPositionAirPlane();
